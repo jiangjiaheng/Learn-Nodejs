6c03e576e63dd232674e66a79f2e035374b4119a
JJH19 <jiangjiaheng1994@qq.com>
2019-11-17 16:53:38 +0800

    add model-sequelize

---
+++ Web/db/model-sequelize/config.js
@@ -0,0 +1,10 @@
+const config = {
+    database: 'test', // 使用哪个数据库
+    dialect: 'mysql',
+    username: 'root', // 用户名
+    password: '', // 口令
+    host: '', // 主机名
+    port: 3306 // 端口号，MySQL默认3306
+};
+
+module.exports = config;
