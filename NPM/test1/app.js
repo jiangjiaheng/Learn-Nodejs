b7ea96cef480f02e605a08b24643752bfce9296d
landscape <jiangjiaheng1994@qq.com>
2021-12-12 14:44:53 +0800

    add npm test

---
+++ NPM/test1/app.js
@@ -0,0 +1,10 @@
+const ProgressBar = require('progress')
+const chalk = import('chalk')
+
+const bar = new ProgressBar(':bar', { total: 10 })
+const timer = setInterval(() => {
+  bar.tick()
+  if (bar.complete) {
+    clearInterval(timer)
+  }
+}, 100)
